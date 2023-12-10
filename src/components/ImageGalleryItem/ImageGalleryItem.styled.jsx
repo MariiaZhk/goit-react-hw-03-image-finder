8a576dea12c592ed1f959022294102ac2e0ddbe0
MariiaZhk <maria8zhuk@gmail.com>
2023-12-10 11:26:45 +0100

    first

--- src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
+++ src/components/ImageGalleryItem/ImageGalleryItem.styled.jsx
@@ -1,11 +1,6 @@
 import styled from 'styled-components';
 
 export const GalleryItemStyled = styled.li`
-  /* margin: 5px;
-  padding: 5px; */
-  /* background-color: #242424;
-  border-radius: 8px; */
-
   border-radius: 2px;
   box-shadow: 0px 1px 3px 0px rgba(0, 0, 0, 0.2),
     0px 1px 1px 0px rgba(0, 0, 0, 0.14), 0px 2px 1px -1px rgba(0, 0, 0, 0.12);
@@ -13,7 +8,7 @@ export const GalleryItemStyled = styled.li`
 
 export const GalleryImgStyled = styled.img`
   width: 100%;
-  height: 250px;
+  height: 260px;
   object-fit: cover;
   border-radius: 4px;
   box-shadow: 0 4px 8px 0 rgba(215, 212, 212, 0.323),
--- src/components/LoadMoreButton/LoadMoreButton.styled.jsx
+++ src/components/LoadMoreButton/LoadMoreButton.styled.jsx
@@ -3,7 +3,7 @@ import styled from 'styled-components';
 export const LoadBtnStyled = styled.button`
   width: 200px;
   height: 50px;
-  margin: 10px auto;
+  margin: 0 auto;
   padding: 10px 16px;
   border-radius: 2px;
   background-color: #5daef1;
